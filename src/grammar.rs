f04f3e70458d619c562eccccff2599452ebb9f36
Shiroy <awacheux.utc@gmail.com>
2016-05-04 21:59:04 +0200

    Implement lexical rule parsing

---
+++ src/grammar.rs
@@ -0,0 +1,253 @@
+/*
+Copyright (c) 2016 Antoine Wacheux
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+pub struct LexicalUnit {
+    name : String,
+    regex : String,
+}
+
+pub struct ProductionRule {
+    name: String,
+    rule: Vec<RuleComponent>
+}
+
+pub enum RuleComponent {
+    ProductionRule(String),
+    LexicalUnit(String)
+}
+
+pub struct Grammar {
+    lexical_units : Vec<LexicalUnit>,
+    production_rules: Vec<ProductionRule>,
+    axiom : String
+}
+
+impl Grammar {
+    fn new() -> Grammar {
+        Grammar {
+            lexical_units: Vec::new(),
+            production_rules: Vec::new(),
+            axiom: String::new(),
+        }
+    }
+}
+
+//Methods
+//get_rule(name: String&) -> Vec<ProductionRule>&
+
+struct ParserContext {
+    src: String,
+    row: u32,
+    col: u32,
+    str_pos: usize,
+}
+
+impl ParserContext {
+    fn new(src : String) -> ParserContext {
+        ParserContext {
+            src: src,
+            row: 1,
+            col: 0,
+            str_pos: 0
+        }
+    }
+
+    fn next_word(&mut self) -> Option<String> {
+        let mut iter = self.src.chars();
+        let mut ch = iter.nth(self.str_pos);
+        let mut c : char;
+        let mut read_word = String::new();
+
+        match ch {
+            None => { return None },
+            Some(chr) => { c = chr}
+        }
+
+        macro_rules! next_char {
+            ($err:stmt) => ({
+                self.str_pos += 1;
+                ch = iter.next();
+
+                match ch {
+                    None => { $err },
+                    Some(chr) => { c = chr }
+                }
+
+                match c {
+                    ' ' | '\t' => self.col += 1,
+                    '\n' => self.row += 1,
+                    _ => {}
+                }
+            });
+        }
+
+        while (c == ' ') || (c == '\n') || (c == '\t') { //We skip leading whitespaces
+            next_char! { return None }
+        }
+
+        while !((c == ' ') || (c == '\n') || (c == '\t')) {
+            read_word.push(c);
+            next_char! { return Some(read_word) }
+        }
+
+        Some(read_word)
+
+        Err(String::from("Not yet implemented. Sorry bro :("))
+    }
+
+    pub fn read_line(&mut self) -> Option<String> {
+        let mut iter = self.src.chars();
+        let mut ch = iter.nth(self.str_pos);
+        let mut c : char;
+        let mut read_word = String::new();
+
+        match ch {
+            None => { return None },
+            Some(chr) => { c = chr}
+        }
+
+        macro_rules! next_char {
+            ($err:stmt) => ({
+                self.str_pos += 1;
+                ch = iter.next();
+
+                match ch {
+                    None => { $err },
+                    Some(chr) => { c = chr }
+                }
+
+                match c {
+                    ' ' | '\t' => self.col += 1,
+                    '\n' => self.row += 1,
+                    _ => {}
+                }
+            });
+        }
+
+        while (c == ' ') || (c == '\n') || (c == '\t') { //We skip leading whitespaces
+            next_char! { return None }
+        }
+
+        while c != '\n' {
+            read_word.push(c);
+            next_char! { return Some(read_word) }
+        }
+
+        Some(read_word)
+    }
+}
+
+struct GrammarParser {
+    ctx: ParserContext,
+    grammar: Grammar
+}
+
+impl GrammarParser {
+    fn new(src: String) -> GrammarParser {
+        GrammarParser {
+            ctx : ParserContext::new(src),
+            grammar: Grammar::new(),
+        }
+    }
+
+    fn get_resulting_grammar(self) -> Grammar {
+        self.grammar
+    }
+
+    fn parse(&mut self) -> Result<(), String> {
+        while let Some(str_word) = self.ctx.next_word() {
+            match str_word.chars().next().unwrap() {
+                'A'...'Z' => {
+                    match self.parse_grammar_rule(str_word) {
+                        Ok(()) => {},
+                        Err(msg) => {return Err(msg)},
+                    }
+                },
+                'a'...'z' => {
+                    match self.parse_lexical_unit(str_word) {
+                        Ok(()) => {},
+                        Err(msg) => {return Err(msg)},
+                    }
+                }
+                _ => { return Err(String::from("Unknown token : ") + str_word.as_str())}
+            }
+        }
+
+        Ok(())
+    }
+
+    fn parse_grammar_rule(&mut self, firstWord : String) -> Result<(), String> {
+        Err(String::from("NYI"))
+    }
+
+    fn parse_lexical_unit(&mut self, name : String) -> Result<(), String> {
+        //Let's read the ':'
+        match self.ctx.next_word() {
+            None => {return Err(format!("line {} : Expected ':'", self.ctx.row))},
+            Some(token) => {
+                if token != ":" {
+                    return Err(format!("line {} : Expected ':' found '{}'", self.ctx.row, token));
+                }
+            }
+        }
+
+        //Ok, now the regex
+        if let Some(regex) = self.ctx.read_line() {
+            let lexical_rule = LexicalUnit {
+                name: name,
+                regex: regex,
+            };
+
+            self.grammar.lexical_units.push(lexical_rule);
+            Ok(())
+        }
+        else {
+            Err(format!("line {} : The regex is expected after ':' but nothing was found", self.ctx.row))
+        }
+    }
+}
+
+pub fn parse_grammar(src : String) -> Result<Grammar, String> {
+    let mut parser = GrammarParser::new(src);
+
+    match parser.parse() {
+        Ok(()) => Ok(parser.get_resulting_grammar()),
+        Err(msg) => Err(msg)
+    }
+}
+
+#[test]
+fn test_next_word() {
+    let mut ctx = ParserContext::new("test and run".to_owned());
+
+    assert_eq!(ctx.next_word().unwrap(), "test");
+    assert_eq!(ctx.next_word().unwrap(), "and");
+    assert_eq!(ctx.next_word().unwrap(), "run");
+}
+
+#[test]
+fn test_read_line() {
+    let mut ctx = ParserContext::new("line 1\nline2\nline3\nline4".to_owned());
+    ctx.next_word();
+
+    assert_eq!(ctx.read_line().unwrap(), "1");
+    assert_eq!(ctx.read_line().unwrap(), "line2");
+    assert_eq!(ctx.read_line().unwrap(), "line3");
+    assert_eq!(ctx.read_line().unwrap(), "line4");
+}
